f7f0f9f85078147cf16f121df2746a4e6fe09c64
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-07-21 09:40:55 +0530

    Used If Statement

---
+++ ControlStatements/IfStatement.java
@@ -0,0 +1,24 @@
+// Getting Input from the User
+// If value is Greater Printing 
+// Entered Value is Greater
+
+import java.util.Scanner;
+
+public class IfStatement {
+    public static void main(String[]args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int a=10;
+        int b=30;
+        
+        int c=a+b;
+        
+        System.out.println("Input Recived");
+
+        if(c > 40) {
+            System.out.println("Greater Value")
+
+        }
+
+    }
+}
