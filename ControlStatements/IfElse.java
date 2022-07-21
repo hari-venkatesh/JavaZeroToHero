6c5e23941712a7260ca9cd2868bef91862ff3a45
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-07-21 09:45:49 +0530

    IfElse Statements

---
+++ ControlStatements/IfElse.java
@@ -0,0 +1,30 @@
+// Getting Input from the User
+// If value is Greater Printing 
+// Entered Value is Greater
+// Else Printing Lower
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[]args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int a=10;
+        int b=30;
+        
+        int c=a+b;
+        
+        System.out.println("Input Recived");
+
+        if(c > 40) {
+            System.out.println("Greater Value");
+        }
+        else {
+            System.out.println("Lower Value");
+        }
+        }
+
+        }
+
+    }
+}
