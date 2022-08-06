c65e2830dcb6b1a7780b78e12be0c4583f653505
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-08-06 21:08:39 +0530

    Code snippets

---
+++ ControlStatements/stacks.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public static void main(){
+        
+    }
+    public boolean isValid(String s) {
+    
+         //Create a stack and add Character by Character
+        Stack<Character> stack = new Stack<>();
+        
+        //Fix '(' and push into stack and when u encounter ')' pop out 
+         for(int i = 0 ; i < s.length() ; i++)
+         {
+            
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+            {
+                stack.push(s.charAt(i));	 
+            }			 
+            else if(stack.size() != 0)
+            {
+                if(stack.peek() == '(' && s.charAt(i) == ')' || stack.peek() == '{' && s.charAt(i) == '}' 
+                  || stack.peek() == '[' && s.charAt(i) == ']')
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    stack.push(s.charAt(i));
+                }
+            }
+            else
+            {
+                stack.push(s.charAt(i));
+            }
+         }
+         
+         return stack.empty() ? true : false;       
+    
+    }
+    }
